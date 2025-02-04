86f737f1c52ddee8e518d5584deb0c65d39d4f89
zenokira <97588167+zenokira@users.noreply.github.com>
2025-02-04 18:40:22 +0900

    Create WaitPulse.cs

---
+++ 20250204/Thread/WaitPulse.cs
@@ -0,0 +1,83 @@
+
+using System.Linq.Expressions;
+
+namespace WaitPulse
+{
+    class Counter
+    {
+        const int LOOP_COUNT = 1000;
+
+        readonly object thisLock;
+        bool lockedCount = false;
+
+        private int count;
+        public int Count
+        {
+            get { return count; }
+        }
+        public Counter()
+        {
+            thisLock = new object();
+            count = 0;
+        }
+
+        public void Increase()
+        {
+            int loopCount = LOOP_COUNT;
+
+            while(loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    while (count > 0 || lockedCount == true) 
+                        Monitor.Wait(thisLock);
+
+                    lockedCount = true;
+                    count++;
+                    lockedCount = false;
+
+                    Monitor.Pulse(thisLock);
+                }
+            }
+        }
+
+        public void Decrease()
+        {
+            int loopCount = LOOP_COUNT;
+
+            while (loopCount-- > 0)
+            {
+                lock (thisLock)
+                {
+                    while (count < 0 || lockedCount == true)
+                        Monitor.Wait(thisLock);
+
+                    lockedCount = true;
+                    count--;
+                    lockedCount = false;
+
+                    Monitor.Pulse(thisLock);
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+
+            Thread incThread = new Thread(new ThreadStart(counter.Increase));
+            Thread decThread = new Thread(new ThreadStart(counter.Decrease));
+
+            incThread.Start();
+            decThread.Start();
+
+            decThread.Join();
+            incThread.Join();
+
+            Console.WriteLine(counter.Count);
+        }
+
+    }
+}
