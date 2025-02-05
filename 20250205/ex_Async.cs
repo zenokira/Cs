4a309f024806f088359b77dfcf0dffa226e4a61e
zenokira <97588167+zenokira@users.noreply.github.com>
2025-02-05 19:18:14 +0900

    Create ex_Async.cs

---
+++ 20250205/ex_Async.cs
@@ -0,0 +1,42 @@
+using System.Runtime.InteropServices;
+
+namespace ex_Async
+{
+    class MainAPP
+    {
+        async static private void MyMethodAsync(int count)
+        {
+            Console.WriteLine("C");
+            Console.WriteLine("D");
+
+            await Task.Run(async () =>
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    Console.WriteLine($"{i}/{count}...");
+                    await Task.Delay(100);
+                }
+            });
+
+            Console.WriteLine("G");
+            Console.WriteLine("H");
+        }
+
+        static void Caller()
+        {
+            Console.WriteLine("A");
+            Console.WriteLine("B");
+
+            MyMethodAsync(3);
+
+            Console.WriteLine("E");
+            Console.WriteLine("F");
+        }
+        static void Main(string[] args)
+        {
+            Caller();
+
+            Console.ReadLine();
+        }
+    }
+}
