0f1083434094e99d7e6bb4064a8fda13b96bc1f5
zenokira <97588167+zenokira@users.noreply.github.com>
2025-02-20 22:04:09 +0900

    Add files via upload

---
+++ 20250220/Minesweeper/MyButton.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    class MyButton : Button
+    {
+        Mine? mine;
+
+        int ArroundMineCount = 0;
+        int NO;
+
+        public MyButton (int n)
+        {
+            ArroundMineCount = 0;
+            NO = n;
+        }
+
+        public bool isEnabled()
+        {
+            return Enabled == true;
+        }
+        public bool isMineHave()
+        {
+            return mine != null;
+        }
+
+        public void LayingMine()
+        {
+            mine = new ();
+        }
+
+        public void AroundMineCountPlus()
+        {
+            ArroundMineCount++;
+        }
+
+        public int GetArroundMineCount()
+        {
+            return ArroundMineCount;
+        }
+
+        public bool isZeroCount()
+        {
+            return ArroundMineCount == 0;
+        }
+
+        public int GetButtonNO()
+        {
+            return NO;
+        }
+
+        public Point GetButtonPoint()
+        {
+            return new Point(NO % 9, NO / 9);
+        }
+    }
+}
