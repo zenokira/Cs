7e623cba3f67959267658892eb6c1925dd700bac
zenokira <97588167+zenokira@users.noreply.github.com>
2025-07-01 19:53:21 +0900

    Add files via upload

---
+++ 20250701/MinesweeperServer/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperServer
+{
+    public class Player
+    {
+        public int Id { get; private set; }
+        public Socket Player_Socket { get; private set; }
+        public PlayerState State { get; set; }
+        public bool HasClicked { get; set; }
+        public int ClearTime { get; set; } = -1;
+
+        public Player(int id, Socket sock)
+        {
+            Id = id;
+            Player_Socket = sock;
+            State = PlayerState.Alive;
+            HasClicked = false;
+        }
+
+        public void Reset()
+        {
+            State = PlayerState.Alive;
+            HasClicked = false;
+        }
+    }
+}
