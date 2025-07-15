82f79417c75c8ed12a58b5f914ebd15aaa227a9a
zenokira <97588167+zenokira@users.noreply.github.com>
2025-07-15 19:57:00 +0900

    Add files via upload

---
+++ 20250715/EchoClient/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EchoClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn_SEND = new Button();
+            label1 = new Label();
+            listBox_MsgList = new ListBox();
+            textBox_MSG = new TextBox();
+            SuspendLayout();
+            // 
+            // btn_SEND
+            // 
+            btn_SEND.Location = new Point(599, 396);
+            btn_SEND.Name = "btn_SEND";
+            btn_SEND.Size = new Size(85, 23);
+            btn_SEND.TabIndex = 0;
+            btn_SEND.Text = "전송";
+            btn_SEND.UseVisualStyleBackColor = true;
+            btn_SEND.Click += btn_SEND_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(51, 42);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // listBox_MsgList
+            // 
+            listBox_MsgList.FormattingEnabled = true;
+            listBox_MsgList.ItemHeight = 15;
+            listBox_MsgList.Location = new Point(38, 90);
+            listBox_MsgList.Name = "listBox_MsgList";
+            listBox_MsgList.Size = new Size(546, 274);
+            listBox_MsgList.TabIndex = 2;
+            // 
+            // textBox_MSG
+            // 
+            textBox_MSG.Location = new Point(38, 396);
+            textBox_MSG.Name = "textBox_MSG";
+            textBox_MSG.Size = new Size(546, 23);
+            textBox_MSG.TabIndex = 3;
+            textBox_MSG.TextChanged += textBox_MSG_TextChanged;
+            textBox_MSG.KeyDown += textBox_MSG_KeyDown;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(837, 547);
+            Controls.Add(textBox_MSG);
+            Controls.Add(listBox_MsgList);
+            Controls.Add(label1);
+            Controls.Add(btn_SEND);
+            Name = "Form1";
+            Text = "Form1";
+            FormClosed += Form1_FormClosed;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn_SEND;
+        private Label label1;
+        private ListBox listBox_MsgList;
+        private TextBox textBox_MSG;
+    }
+}
---
+++ 20250715/EchoClient/NetWork.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EchoClient
+{
+    internal class NetWork
+    {
+        readonly string[] COMMAND = { "exit", "make", "number", "remove" , "error" , "socket"};
+        const string SERVER_IP = "127.0.0.1";
+        const int SERVER_PORT = 9000;
+        const int BUFSIZE = 512;
+
+        const int COMMAND_START_IDX = 0;
+        const int COMMAND_LENGTH = 10;
+
+        const int SOCKET_NO_START_IDX = COMMAND_START_IDX + COMMAND_LENGTH;
+        const int SOCKET_NO_LENGTH = 2;
+
+        const int DATA_START_IDX = SOCKET_NO_START_IDX + SOCKET_NO_LENGTH;
+        const int DATA_LENGTH = 512 - COMMAND_LENGTH - SOCKET_NO_LENGTH;
+
+       
+        Socket socketClient;
+        int socketNo = -1;
+        public NetWork()
+        {
+            
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socketClient.Connect(SERVER_IP, SERVER_PORT);
+                Form1 form = Application.OpenForms["Form1"] as Form1; 
+                Thread thread = new Thread(() => ReceiveMessage());
+                thread.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[클라이언트] 예외 발생: {0}", ex.Message);
+            }
+        }
+        // 송신
+        public void SendMessage(string data, int No)
+        {
+            
+            string strData = SetMessageFormat(data, No);
+            byte[] msg = Encoding.UTF8.GetBytes(strData);
+            socketClient.Send(msg);
+        }
+
+        string SetMessageFormat(string data , int No)
+        {
+            //"exit", "make", "number", "remove" , "error" , "socket"
+            string[] split = data.Split(" ");
+
+            if (isCommand(split[0])) return data;
+            else
+                return $"chat {No} {data} ";
+            /*
+            string sendData = split[0] switch
+            {
+                _ => $"chat {No} " + data
+            };
+            */
+           
+        }
+
+        public void disConnect()
+        {
+            socketClient.Close();
+        }
+        public bool isConnect()
+        {
+            return socketClient.Connected;
+        }
+             //수신
+        public void ReceiveMessage()
+        {
+            Form1 form = Application.OpenForms["Form1"] as Form1;
+            if (form == null) return;
+            while (true) { 
+                byte[] recvBytes = new byte[BUFSIZE];
+                int recvLen = 0;
+                try
+                {
+                    recvLen = socketClient.Receive(recvBytes);
+                }
+                catch (SocketException ex)
+                {
+                    form.send2MainThread("서버가 비정상적으로 종료되었습니다.");
+                    socketClient.Close();
+                    break;
+                }
+                
+                if(recvLen == 0)
+                    break;
+
+                string data = Encoding.Default.GetString(recvBytes);
+                data = data.Trim('\0');
+
+                form.send2MainThread(data);
+               
+                
+                if (isExit(data))
+                {
+                    form.send2MainThread("정상적으로 서버와 연결이 종료되었습니다.");
+                    break;
+                }
+            }
+        }
+
+ 
+        bool isExit(string data)
+        {
+            return data.Equals("exit");
+        }
+
+        bool isCommand(string msg)
+        {
+            foreach (string str in COMMAND)
+            {
+                if (msg.StartsWith(str)) return true;
+            }
+            return false;
+        }
+
+        public int GetSockNo()
+        {
+            return socketNo;
+        }
+
+        public void SetSockNo(int n)
+        {
+            socketNo = n;
+        }
+      
+    }
+}
+
