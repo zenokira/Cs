8e860d192d495159ac83fd0067e02ccd2319224a
zenokira <97588167+zenokira@users.noreply.github.com>
2025-09-01 20:01:45 +0900

    Add files via upload

---
+++ Game_2048/20250901/2048/GameMain.cs
@@ -0,0 +1,17 @@
+namespace _2048
+{
+    public partial class GameMain : Form
+    {
+        public GameMain()
+        {
+            InitializeComponent();
+        }
+
+        private void GameMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
+
---
+++ Game_2048/20250901/2048/NumberTile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2048
+{
+
+    class NumberTile
+    {
+        private int Number { get; set; }
+
+        
+        private int X { get; set; }  // X 좌표  0 <= X <= 3
+        private int Y { get; set ; } // Y 좌표  0 <= Y <= 3
+
+
+
+
+    }
+}
