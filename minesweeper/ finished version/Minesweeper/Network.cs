bb0eb198b1043825932b6ca2be7455c80d9bddc7
zenokira <97588167+zenokira@users.noreply.github.com>
2025-09-01 20:00:10 +0900

    Add files via upload

---
+++ minesweeper/ finished version/Minesweeper/Network.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace Minesweeper
+{
+    public class Network
+    {
+        const string SERVER_IP = "127.0.0.1";
+        const int SERVER_PORT = 9900;
+        readonly int BUFSIZE = 1024;
+
+        Socket socketClient;
+
+        readonly string START_WORD = "<SOW>";
+        readonly string END_WORD = "<EOW>";
+
+
+        bool isReceiving = false;
+        public Network()
+        {
+
+        }
+        public void Dispose()
+        {
+            Disconnect();
+        }
+
+        public bool IsSocketNULL()
+        {
+            return socketClient == null;
+        }
+
+        public bool IsConnected()
+        {
+            return socketClient.IsBound;
+        }
+
+        public bool Connect()
+        {
+            try
+            {
+                socketClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socketClient.Connect(SERVER_IP, SERVER_PORT);
+                socketClient.ReceiveTimeout = 500;
+                isReceiving = true;
+
+                Thread thread = new Thread(() => ReceiveMessage()) { IsBackground = true };
+                thread.Start();
+                return true;
+            }
+            catch (SocketException ex)
+            {
+                return false;
+            }
+        }
+
+        public bool IsReallyConnected()
+        {
+            if (socketClient == null)
+                return false;
+            try
+            {
+                return !(socketClient.Poll(1, SelectMode.SelectRead) && socketClient.Available == 0);
+            }
+            catch (ObjectDisposedException ode)
+            {
+                // 소켓이 이미 Dispose된 상태
+                return false;
+            }
+            catch (SocketException se)
+            {
+                return false;
+            }
+        }
+
+        public void SendMessage<T>(CommandType command, int sendNo, int recvNo, T action, string data, string func) where T : Enum
+        {
+            string str_action = action.ToString();
+            Console.WriteLine($"{func} 에서 호출됨");
+            string sendData = $"{START_WORD}{command} {sendNo} {recvNo} {str_action} {data}{END_WORD}";
+            byte[] sendMsg = Encoding.UTF8.GetBytes(sendData);
+            socketClient.Send(sendMsg);
+        }
+
+        public void Disconnect()
+        {
+            isReceiving = false;
+
+            if (socketClient == null) return;
+
+            try { socketClient.Shutdown(SocketShutdown.Both); } catch { }
+            try { socketClient.Close(); } catch { }
+            try { socketClient.Dispose(); } catch { }
+
+            socketClient = null;
+        }
+
+        void ReceiveMessage()
+        {
+            지뢰찾기 form = Application.OpenForms["지뢰찾기"] as 지뢰찾기;
+            if (form == null) return;
+
+
+            while (isReceiving)
+            {
+                if (socketClient == null || !socketClient.Connected)
+                {
+                    // 소켓이 끊기면 루프 종료
+                    break;
+                }
+
+                byte[] recvBytes = new byte[BUFSIZE];
+                int recvLen = 0;
+                try
+                {
+                    recvLen = socketClient.Receive(recvBytes);
+                    if (recvLen <= 0) break;
+                    string recvData = Encoding.Default.GetString(recvBytes);
+                    recvData = recvData.Trim('\0');
+                    form.net2MainThread(recvData);
+                }
+                catch (SocketException ex)
+                {
+                    if (ex.SocketErrorCode == SocketError.TimedOut) continue;
+                }
+                catch (ObjectDisposedException ex)
+                {
+                    // 소켓이 이미 닫혔다면 그냥 종료
+                    break;
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"ReceiveMessage 예외: {ex}");
+                    isReceiving = false;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ minesweeper/ finished version/MinesweeperServer/Player.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperServer
+{
+    public class Player
+    {
+        public int Id { get; private set; }
+        public bool IsConnected { get; set; } = true;
+        public Socket Player_Socket { get; private set; }
+        public PlayerState State { get; set; }
+        public bool HasClicked { get; set; }
+        public int ClearTime { get; set; } = -1;
+
+        public bool ExitReserved { get; set; } = false;
+        public bool LeaveReserved { get; set; } = false;
+        public Player(int id, Socket sock)
+        {
+            Id = id;
+            Player_Socket = sock;
+            Player_Socket.ReceiveTimeout = 500;
+            State = PlayerState.Alive;
+            HasClicked = false;
+        }
+
+        public void Reset()
+        {
+            State = PlayerState.Alive;
+            HasClicked = false;
+            LeaveReserved = false;
+        }
+
+        public int IsReservation()
+        {
+            if (LeaveReserved) { return 1; }
+            else if (ExitReserved) { return 2; }
+            else { return 0; }
+        }
+
+        public bool DisconnectPlayer()
+        {
+            if (Player_Socket != null)
+            {
+                try
+                {
+                    Player_Socket.Shutdown(SocketShutdown.Both);
+                }
+                catch (SocketException) { }  // 이미 끊겼으면 무시
+                catch (ObjectDisposedException) { }
+
+                try { Player_Socket.Close(); } catch { return false; }
+                try { Player_Socket.Dispose(); } catch { return false; }
+
+                Player_Socket = null;
+                IsConnected = false;
+
+                Console.WriteLine($"[Server] 플레이어 {Id}의 소켓 연결을 서버에서 정리했습니다.");
+                
+                return true;
+            }
+            return false;
+        }
+    }
+}
