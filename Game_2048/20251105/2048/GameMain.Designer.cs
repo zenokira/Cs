412f39bdeb11bd95d246046c2580a822fe77c511
zenokira <97588167+zenokira@users.noreply.github.com>
2025-11-05 21:57:43 +0900

    Add files via upload

---
+++ Game_2048/20251105/2048/GameMain.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _2048
+{
+    partial class GameMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            btn_start = new Button();
+            timer_save = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // btn_start
+            // 
+            btn_start.Location = new Point(372, 22);
+            btn_start.Name = "btn_start";
+            btn_start.Size = new Size(84, 37);
+            btn_start.TabIndex = 0;
+            btn_start.Text = "게임 시작";
+            btn_start.UseVisualStyleBackColor = true;
+            btn_start.Click += btn_start_Click;
+            // 
+            // timer_save
+            // 
+            timer_save.Tick += timer_save_Tick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(38, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(78, 38);
+            button1.TabIndex = 1;
+            button1.Text = "테스트용";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // GameMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(484, 561);
+            Controls.Add(button1);
+            Controls.Add(btn_start);
+            Name = "GameMain";
+            Text = "2048";
+            FormClosing += GameMain_FormClosing;
+            Load += GameMain_Load;
+            Paint += GameMain_Paint;
+            KeyDown += GameMain_KeyDown;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btn_start;
+        private System.Windows.Forms.Timer timer_save;
+        private Button button1;
+    }
+}
